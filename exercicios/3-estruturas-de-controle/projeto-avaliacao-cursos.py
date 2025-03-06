c7bf85c4fa9dca3f9dd2279bc63ec0214836c137
fleandhro <66805446+fleandhro@users.noreply.github.com>
2025-03-06 21:35:54 +0000

    correcao

--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,34 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+
+cursos_linkedin = ['C++', 'Python', 'Cobol', 'Pascal', 'Basic']
+
+curso_java = 'Java'
+curso_pascal = 'Pascal'
+curso_basic = 'Basic'
+
+avaliacoes = {}
+
+if curso_java in cursos_linkedin:
+  print(f'Curso {curso_java} no catálogo. Por favor, avalie.')
+  avaliacoes[curso_java] = int(input('Nota (0 a 5): '))
+else:
+  print(f'Curso {curso_java} não listado.')
+
+if curso_pascal in cursos_linkedin:
+  print(f'Curso {curso_pascal} no catálogo. Por favor, avalie.')
+  avaliacoes[curso_pascal] = int(input('Nota (0 a 5): '))
+else:
+  print(f'Curso {curso_pascal} não listado.')
+
+if curso_basic in cursos_linkedin:
+  print(f'Curso {curso_basic} no catálogo. Por favor, avalie.')
+  avaliacoes[curso_basic] = int(input('Nota (0 a 5): '))
+else:
+  print(f'Curso {curso_basic} não listado.')
+
+print('')
+print('Avaliações: ')
+print(avaliacoes)
+
